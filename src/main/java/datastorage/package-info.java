8d439829764c9fce06f170e7f7e2a2503b452478
Isabel Janez <isabel@janez.org>
2021-08-20 13:05:49 +0200

    Creating package comments

--- src/main/java/datastorage/package-info.java
+++ src/main/java/datastorage/package-info.java
@@ -1,5 +1,7 @@
 /**
- * 
+ * This package contains the methods for the data storage process by using the Maven dependencies and integrating JSON-B.
+ * It includes the actions of saving the coordination of the farmer and his farmer equipment.
+ * Also it deals with saving the cash value, gas amount and the level of difficulty.
  */
 /**
  * @author Isabel
--- src/main/java/gui/package-info.java
+++ src/main/java/gui/package-info.java
@@ -1,5 +1,6 @@
 /**
- * 
+ * This package is implementing the graphical user interface.
+ * It contains "View", "Controller" and "Model".
  */
 /**
  * @author 
