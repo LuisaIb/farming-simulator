08e40385807e9f8a76eb0af811517e0123863a5d
Luisa Ibele <luisa.ibele@gmail.com>
2021-08-20 18:47:54 +0200

    Adding instance variables like cash and filling to the Game class.

--- src/main/java/simulator/Game.java
+++ src/main/java/simulator/Game.java
@@ -1,5 +1,27 @@
 package simulator;
 
 public class Game {
+	private int cash;
+	private int filling;
+	
+	public Game(int cash, int filling, int level) {
+		cash = 0; 
+		filling = 100; 
+	}
 
+	public int getCash() {		
+		return cash;
+	}
+
+	public void setCash(int cash) {
+		this.cash = cash;
+	}
+
+	public double getFilling() {
+		return filling;
+	}
+
+	public void setFilling(int filling) {
+		this.filling = filling;
+	}
 }
