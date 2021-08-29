6403e82cb3f2c962e2eba4854ea55775f35bc47c
Luisa Ibele <luisa.ibele@gmail.com>
2021-08-29 21:09:44 +0200

    creating some methods for the GameBoard class

--- src/main/java/gameboard/GameBoard.java
+++ src/main/java/gameboard/GameBoard.java
@@ -1,5 +1,28 @@
 package gameboard;
 
+import gameboard.tiles.Tile;
+
 public class GameBoard {
+	
+	private int width = 30, height = 20;
+	private int [][] tiles;
+	
+	
+	public GameBoard() {
+		tiles = new int[width][height]; 
+		
+	}
+	/*
+	 * checking or setting each tile of the gameboard
+	 */
+	public Tile getTile(int x, int y) {
+		// Tile t = Tile.tiles[tiles[x][y]]; -> setting each tile
+		
+//		if (t == null) {
+//		//	return Tile.forestTile;  -> setting a default tile
+//		}
+//		return t;
+		return null; 
+	}
 
 }
