aa5d57f6ec7e1b6a96fa67960c8cd747de323d70
Luisa Ibele <luisa.ibele@gmail.com>
2021-08-31 13:41:50 +0200

    adding constructor and methods to the Tile class

--- src/main/java/gameboard/tiles/Tile.java
+++ src/main/java/gameboard/tiles/Tile.java
@@ -7,5 +7,34 @@ package gameboard.tiles;
  *
  */
 public class Tile {
+	
+	private final int id; // id for recognizing each kind of tile in the field
+	
+	/**
+	 * the constructor is there to set the id
+	 * 
+	 * @param id
+	 */
+	public Tile(int id) {
+		this.id = id; 
+	}
+	
+	/**
+	 * this method is supposed to get the id of each tile
+	 * @return
+	 */
+	public int getId() {
+		return id;
+	}
+	
+	/**
+	 * this method is supposed to check if the farmer can run over 
+	 * a specific tile or not.
+	 * 
+	 * @return a boolean variable to show if it's possible or not to run over the field
+	 */
+	public boolean isSolid() {
+		return false; 
+	}
 
 }
