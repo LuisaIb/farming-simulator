9d7da22941cb0654c8277de2f91bb875da2c7c49
Luisa Ibele <luisa.ibele@gmail.com>
2021-09-01 09:21:39 +0200

    adjusting the tile with building class with constructor + method

--- src/main/java/gameboard/tiles/TileWithBuilding.java
+++ src/main/java/gameboard/tiles/TileWithBuilding.java
@@ -1,5 +1,29 @@
 package gameboard.tiles;
 
+/**
+ * this class represents a tile with a building on it on the gameboard
+ * @author Luisaibele
+ *
+ */
 public class TileWithBuilding extends Tile{
+
+	/**
+	 * the constructor sets the id of each tile with a building
+	 * @param id
+	 */
+	public TileWithBuilding(int id) {
+		super(id);
+		
+	}
+	
+	/**
+	 * this method checks if the farmer can run over a tile with a building on it 
+	 * and returns the equal boolean value
+	 */
+	@Override
+	public boolean isSolid() {
+		return false;
+	}
+	
 
 }
