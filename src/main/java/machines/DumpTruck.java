ba19590fc4af566c454fe7737f4066d0cc765720
BS <BS@Zeus2>
2021-09-02 15:29:14 +0200

    Create classes for machines

--- src/main/java/machines/DumpTruck.java
+++ src/main/java/machines/DumpTruck.java
@@ -1,31 +1,40 @@
 package machines;
 
-
 /**
- * 
+ * This class implements the methods, that are needed for a DumpTruck.
  * 
  * @author Hanna
  */
 
-public class DumpTruck extends Machine{
-
+public class DumpTruck extends Machine {
+	
 	private int capacity;
 	private int fillLevel;
 	
-	public DumpTruck(int capacity) {
-		this.capacity = capacity;
+	/**
+	 * the constructor sets the harvest fill level of the Dump Truck
+	 * @param fillLevel 
+	 */
+	public DumpTruck(int fillLevel) {
+		this.fillLevel = fillLevel;
 		
 	}
 	
+	/**
+	 * this method loads the Dump Truck with harvest
+	 */
 	public void load() {
 		if(fillLevel < capacity) {
 			fillLevel++;	
 		} 
-		else {
-			unload();
+		else if(fillLevel == capacity) {
+			System.out.println("The Dump Truck must be unloaded!");
 		}
 	}
 	
+	/**
+	 * this method unloads the harvest in the Dump Truck into the Silo or the Land Trade
+	 */
 	public void unload() {
 		fillLevel = 0;
 	}
