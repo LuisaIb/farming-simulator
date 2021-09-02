36bba8dd9d902c098ca6bb750093d26edb8b9700
BS <BS@Zeus2>
2021-09-02 11:50:43 +0200

    create DumpTruck and Machine

--- src/main/java/machines/DumpTruck.java
+++ src/main/java/machines/DumpTruck.java
@@ -1,5 +1,35 @@
 package machines;
 
-public class DumpTruck {
 
-}
+/**
+ * 
+ * 
+ * @author Hanna
+ */
+
+public class DumpTruck extends Machine{
+
+	private int capacity;
+	private int fillLevel;
+	
+	public DumpTruck(int capacity) {
+		this.capacity = capacity;
+		
+	}
+	
+	public void load() {
+		if(fillLevel < capacity) {
+			fillLevel++;	
+		} 
+		else {
+			unload();
+		}
+	}
+	
+	public void unload() {
+		fillLevel = 0;
+	}
+	
+	
+
+	}
