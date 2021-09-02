faf0cabd1b067d1a0a4c00a4e545c8c0cffaa5ac
BS <BS@Zeus2>
2021-09-02 15:09:01 +0200

    create Harvester and Machine class

---
+++ src/main/java/machines/Harvester.java
@@ -0,0 +1,11 @@
+package machines;
+
+/**
+ * This class implements ...
+ * 
+ * @author Hanna
+ */
+
+public class Harvester {
+
+}
--- src/main/java/machines/Machine.java
+++ src/main/java/machines/Machine.java
@@ -1,5 +1,58 @@
 package machines;
 
+/**
+ * This class implements the methods, that are basic functions for every Machine.
+ * 
+ * @author Hanna
+ */
+
 public class Machine {
 
+	private double x;
+	private double y;
+	
+	public Machine() {
+	}
+	
+	/**
+	 * the constructor sets the position in the coordinate system
+	 * @param id
+	 */
+	public Machine(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+    /** 
+     * hand over the x-Coordinate
+     * @return x
+     */
+	public double getX() {
+		return x;
+	}
+	
+    /** 
+     * Sets the x-Coordinate
+     * @param x - Coordinate
+     */
+	public void setX(double x) {
+		this.x = x;
+	}
+	
+    /** 
+     * hand over the y-Coordinate
+     * @return y
+     */
+	public double getY() {
+		return y;
+	}
+	
+    /** 
+     * Sets the y-Coordinate
+     * @param y - Coordinate
+     */
+	public void setY(double y) {
+		this.y = y;
+	}
+	
 }
