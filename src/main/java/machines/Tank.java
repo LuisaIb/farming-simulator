cfff0dd238e4a200891fff0a22ef2c21516a2c4a
BS <BS@Zeus2>
2021-09-02 16:14:16 +0200

    change of class Tank

--- src/main/java/machines/Tank.java
+++ src/main/java/machines/Tank.java
@@ -2,88 +2,32 @@ package machines;
 
 public class Tank{
 	 
-	int tankvolumen;
-    int benzinstand;
-    int verbrauch;
-    int fields;
-
-    public Tank(int benz, int fields)
-    {  
-        tankvolumen = 150;
-        benzinstand = benz;
-
+	int petrolCapacity;
+    private int petrolLevel;
+    private int y;
+    private int x;
+ 
+    public Tank(int petrolLevel, int x, int y) {  
+    	this.petrolLevel = petrolLevel;
+    	this.x = x;
+    	this.y = y;
     }
     
-    public int Tanken(int liter)
-    {
-        int differenz = tankvolumen - benzinstand;
-        if(liter <= differenz)
-        {
-            benzinstand = benzinstand + liter;
-            int zaehler = 0;
-            while (zaehler < liter)
-            {
-                System.out.print("*");
-                try 
-                {
-                    Thread.sleep(1000);
-                } 
-                catch (InterruptedException e)
-                {
-                    e.printStackTrace();
-                } 
-                zaehler++;
-            }
-            System.out.println("");
-            System.out.println("Sie haben " +liter +" Liter getankt.");
-            System.out.println("Ihr Benzinstand beträgt nun " +benzinstand +" Liter.");
-            System.out.println("");
-        }
-        if(liter > differenz)
-        {
-            benzinstand = benzinstand + differenz;
-            int zaehler = 0;
-            while (zaehler < differenz)
-            {
-                System.out.print("*");
-                try 
-                {
-                    Thread.sleep(1000);
-                } 
-                catch (InterruptedException e)
-                {
-                    e.printStackTrace();
-                } 
-                zaehler++;
-            }
-            System.out.println("");
-            System.out.println("Brechen Sie den Tankvorgang ab!");
-            System.out.println("Sie haben " +differenz +" Liter getankt.");
-            System.out.println("Ihr Tank ist voll.");
-            System.out.println("");
-        }
+    public int move(int petrolLevel, int x, int y) {
+    	if(x == +-1 || y == +-1) {
+    		petrolLevel--;
+    	} return petrolLevel;
     }
     
-    public int consumption(int fields, int liter) {
-    	for(int i=0; i<=150 && i>=0; i--) {
-    		
+    public void lowPetrolLevel(int petrolLevel) {
+    	if(petrolLevel <= 50) {
+    		System.out.println("The petrol level is only " + petrolLevel + "l.");
+    		System.out.println("Please refuel your vehicle!");
+    } 	else if (petrolLevel == 0) {
+    		System.out.println("The tank is empty!");
+    		System.out.println("Game Over!");
     }
-    
-    public void move(int fields)
-    {
-        int verb;
-        benzinstand = benzinstand - verb;
-        if((benzinstand <= 10)&&(benzinstand > 0))
-        {
-            System.out.println("Sie haben noch maximal 10 Liter Benzin.");
-            System.out.println("Tanken Sie bitte ihr Fahrzeug auf!");
-            System.out.println("");
-        }
-        if(benzinstand == 0)
-        {
-            System.out.println("Ihr Tank ist leer.");
-        }
     }
 }
-}
+
 
