dcd9dbf3c8311eb542213ad29034ae737c76ff7a
BS <BS@Zeus2>
2021-09-04 13:08:22 +0200

    change working devices

--- src/main/java/machines/Cultivator.java
+++ src/main/java/machines/Cultivator.java
@@ -2,4 +2,8 @@ package machines;
 
 public class Cultivator extends Machine {
 
+	public void wrapField() {
+		//set field from harvested to ready to harvest
+	}
+	
 }
--- src/main/java/machines/SeedDrill.java
+++ src/main/java/machines/SeedDrill.java
@@ -1,5 +1,9 @@
 package machines;
 
 public class SeedDrill extends Machine {
+	
+	public void seedGrain() {
+		//set field from ready to harvest to growth stage 0
+	}
 
 }
