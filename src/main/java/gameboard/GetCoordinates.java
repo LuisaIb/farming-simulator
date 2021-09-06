68b2fc16f152d5523744fb8843ce34b40f136e42
Luisa Ibele <luisa.ibele@gmail.com>
2021-09-06 12:24:42 +0200

    adding a test hashmap for the coordinates

--- src/main/java/gameboard/GetCoordinates.java
+++ src/main/java/gameboard/GetCoordinates.java
@@ -3,10 +3,20 @@
  */
 package gameboard;
 
+import java.util.HashMap;
+
 /**
- * @author 
+ * 
+ * @author Luisaibele
  *
  */
 public class GetCoordinates {
 
+	/* creating a hashmap for the gameboard itself with the first value (int) representing an index for each tile 
+	* and the second value representing a boolean for checking if each tile is solid or not.
+	*/
+	private HashMap<Integer, Boolean> board = new HashMap<>();
+	
+	private int index; //values: 0-600 the index for each tile on the board
+	
 }
