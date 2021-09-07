7874e356c469f5baa53c538b9973c220db3fa0ef
Luisa Ibele <luisa.ibele@gmail.com>
2021-09-07 09:54:10 +0200

    creating a class for the court Trade

---
+++ src/main/java/gameboard/tiles/CourtTrade.java
@@ -0,0 +1,25 @@
+package gameboard.tiles;
+
+/**
+ * this class represents the Court Trade of the game
+ * @author Luisaibele
+ *
+ */
+public class CourtTrade extends TileWithBuilding{
+
+	/**
+	 * the constructor is there to set the id of the Court Trade
+	 * @param id
+	 */
+	public CourtTrade(int id) {
+		super(id);
+		
+	}
+	/**
+	 * this method represents the functionality to sell grain to customers
+	 */
+	public void sellGrain() {
+		
+	}
+
+}
