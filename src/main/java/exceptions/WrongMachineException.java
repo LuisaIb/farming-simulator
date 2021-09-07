281a5aa2db00ab3274e7cdf71ffb610928cb2623
Luisa Ibele <luisa.ibele@gmail.com>
2021-09-07 14:20:00 +0200

    adding comments

--- src/main/java/exceptions/WrongMachineException.java
+++ src/main/java/exceptions/WrongMachineException.java
@@ -1,5 +1,11 @@
 package exceptions;
 
+/**
+ * this class represents an exception, which is thrown, 
+ * when wanting to use the wrong machine for a specific action.
+ * @author Luisaibele
+ *
+ */
 public class WrongMachineException extends Exception{
 
 }
