ee0cd41ad6d3c0423de5a54bd4e20b55650cfdac
Luisa Ibele <luisa.ibele@gmail.com>
2021-09-07 09:54:25 +0200

    changes on the silo class

--- src/main/java/gameboard/tiles/Silo.java
+++ src/main/java/gameboard/tiles/Silo.java
@@ -1,8 +1,17 @@
 package gameboard.tiles;
 
+/**
+ * this class represents the Silo for storage of the grain
+ * @author Luisaibele
+ *
+ */
 public class Silo extends TileWithBuilding{
 	private int capacity;
 	
+	/**
+	 * constructor for the Silo
+	 * @param id
+	 */
 	public Silo(int id) {
 		super(id);
 	}
