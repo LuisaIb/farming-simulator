bbee27e43171e4d17e70057f5be90ae699c048e5
Luisa Ibele <luisa.ibele@gmail.com>
2021-09-07 16:01:55 +0200

    adjusting move method

--- src/main/java/gameboard/objects/MovingObject.java
+++ src/main/java/gameboard/objects/MovingObject.java
@@ -6,12 +6,23 @@ package gameboard.objects;
  *
  */
 public class MovingObject {
+	private int x;
+	private int y;
+	private int xMove;
+	private int yMove;
 	// to be implemented
 
+	
 	/**
-	 * this method is representing the functionality of each object to move
+	 * this method is representing the functionality of each object 
+	 * to move over the gameboard
 	 */
-	public void move() {
-		// to be implemented
+	public void move(int x, int y) {
+		/*
+		 * to be implemented
+		 * checking with keyboard input -> x/yMove
+		 * => influence on position (x/y)
+		 * OR: solution with hashmap and index
+		 */
 	}
 }
