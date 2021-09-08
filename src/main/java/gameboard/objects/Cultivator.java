46fb12f1b251503390585f61d018283211de8409
Luisa Ibele <luisa.ibele@gmail.com>
2021-09-08 13:18:29 +0200

    adding comments

--- src/main/java/gameboard/objects/Cultivator.java
+++ src/main/java/gameboard/objects/Cultivator.java
@@ -1,7 +1,10 @@
 package gameboard.objects;
 
+
 /**
- * @author 
+ * this class represents the device Cultivator, 
+ * which is supposed to wrap a field when it's read to do so
+ * @author Luisaibele, Hanna
  *
  */
 public class Cultivator extends WorkingDevice {
