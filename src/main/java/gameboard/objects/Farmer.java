3a1c7396753e5784cd06a897b96b9df0602f4550
Luisa Ibele <luisa.ibele@gmail.com>
2021-09-08 13:20:20 +0200

    adding comments

--- src/main/java/gameboard/objects/Farmer.java
+++ src/main/java/gameboard/objects/Farmer.java
@@ -1,13 +1,15 @@
 package gameboard.objects;
 
 /**
- * @author 
+ * this class represents the farmer (= player) of our game
+ * @author Luisaibele
  *
  */
 public class Farmer extends MovingObject{
 
 	/**
-	 * 
+	 * this methode represents the functionality of the farmer 
+	 * to enter a machine
 	 */
 	public void enterMachine() {
 		
