91c3eb950fa0540c1d46212c0133fe564562238a
Luisa Ibele <luisa.ibele@gmail.com>
2021-09-08 16:39:34 +0200

    trying to add a timer for growing of the field

--- src/main/java/gameboard/tiles/FieldTile.java
+++ src/main/java/gameboard/tiles/FieldTile.java
@@ -1,11 +1,17 @@
 package gameboard.tiles;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * This class represents a tile, which is a field with its several properties
  * @author Luisaibele
  *
  */
 public class FieldTile extends Tile{
+	
+	private Timer timer = new Timer();
+	
 	private int growthState; //representing the state of growth of the first field
 	private int growthState2; //representing the state of growth of the second field
 	private int growthState3; //representing the state of growth of the third field
@@ -26,7 +32,7 @@ public class FieldTile extends Tile{
 	 */
 	public FieldTile(int id, int growthState, int growthState2, int growthState3 ) {
 		super(id);
-		this.growthState = growthState;
+//		this.growthState = growthState;
 		this.growthState2 = growthState2;
 		this.growthState3 = growthState3;
 		
@@ -63,8 +69,25 @@ public class FieldTile extends Tile{
 	 * this method sets the growthState of the first field
 	 * @param growthState
 	 */
-	public void setGrowthState(int growthState) {
+	public void setGrowthState(int growthState, Timer timer) {
 		this.growthState = growthState;
+		// trying a timer for the growing
+		if(growthState == 1) {
+			System.out.println("The first field is growing now!");
+		}
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				if(growthState == 1 || growthState ==2 || growthState ==3) {
+//					growthState = growthState+1;
+					} else if(growthState == 4) {
+						System.out.println("The first field is ready to harvest now!");
+						timer.cancel();
+					}	
+			}
+		}, 2*1000, 2*1000);
+		
+		// if else statement without timer for growing
 		if(growthState == 4) {
 			System.out.println("The first field is ready to harvest!");
 			//timer.stop
