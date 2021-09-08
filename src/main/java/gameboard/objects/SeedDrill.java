1a09e34dee10827002d97c0cd10899d69b789ca7
Luisa Ibele <luisa.ibele@gmail.com>
2021-09-08 13:26:21 +0200

    adding comments to the seed drill class

--- src/main/java/gameboard/objects/SeedDrill.java
+++ src/main/java/gameboard/objects/SeedDrill.java
@@ -1,14 +1,16 @@
 package gameboard.objects;
 
 /**
- * @author 
+ * this class represents the device SeedDrill with its functionality
+ * to seed a field.
+ * @author Luisaibele
  *
  */
 public class SeedDrill extends WorkingDevice {
 	// private int id = 1; -> for attachment hashmap
 	
 	/**
-	 * 
+	 * this method is there to seed a field or to set it to growth stage 0
 	 */
 	public void seedGrain() {
 		//set field from ready to harvest to growth stage 0
