ce6ea2b87a8f91bd7b96450cb5cc3070fe08ffea
Judith Romer <romer.j@stud.dhbw-ravensburg.de>
2021-09-15 08:52:56 +0200

    deleting unnecessary imports and adding comments

--- src/main/java/gui/model/GameInformation.java
+++ src/main/java/gui/model/GameInformation.java
@@ -1,62 +1,57 @@
 package gui.model;
 
 import gameboard.GameValue;
-import gameboard.objects.Farmer;
 import gameboard.objects.Harvester;
 import gameboard.objects.Tractor;
 import gameboard.tiles.Silo;
 import gui.view.InformationBox;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 
+/**
+ * This class adds listeners to all the Properties that are shown in the gameScene. With the help of this class nobody
+ * has to care that the actual values are shown.
+ */
 public class GameInformation {
     private InformationBox informationBox;
 
+    /**
+     * This method constructs an object of the class GameInformation. When it is called it gets the actual
+     * objects handed over, and it starts to synchronize the information by calling the method
+     * synchronizeInformation().
+     *
+     * @param informationBox - the informationBox of the actual gameScene to get access to the newsField
+     * @param gameValue - the actual object of the class GameValue to get access to the properties
+     * @param tractor - the actual object of the class Tractor to get access to the property
+     * @param harvester - the actual object of the class Harvester to get access to the property
+     * @param silo - the actual object of the class Silo to get access to the property
+     */
     public GameInformation(InformationBox informationBox, GameValue gameValue, Tractor tractor, Harvester harvester,
                            Silo silo) {
         this.informationBox = informationBox;
-        bindInformation(gameValue, tractor, harvester, silo);
+        synchronizeInformation(gameValue, tractor, harvester, silo);
     }
 
-    private void bindInformation(GameValue gameValue, Tractor tractor, Harvester harvester, Silo silo){
-        gameValue.cashProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-                informationBox.getMoneyField().setText(gameValue.getCashAsString());
-            }
-        });
+    /**
+     * This method implements listeners to synchronize the information shown in the gameScene with the properties.
+     *
+     * @param gameValue - the actual object of the class GameValue to get access to the properties
+     * @param tractor - the actual object of the class Tractor to get access to the property
+     * @param harvester - the actual object of the class Harvester to get access to the property
+     * @param silo - the actual object of the class Silo to get access to the property
+     */
+    private void synchronizeInformation(GameValue gameValue, Tractor tractor, Harvester harvester, Silo silo){
+        gameValue.cashProperty().addListener((observableValue, number, t1) ->
+                informationBox.getMoneyField().setText(gameValue.getCashAsString()));
 
-        gameValue.day().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-                informationBox.getTimeField().setText(gameValue.getDayAsString());
-            }
-        });
+        gameValue.day().addListener((observableValue, number, t1) ->
+                informationBox.getTimeField().setText(gameValue.getDayAsString()));
 
-        tractor.petrolTankFillLevel().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-                informationBox.getTractorField().setText(tractor.getPetrolTankFillLevelAsString());
-            }
-        });
+        tractor.petrolTankFillLevel().addListener((observableValue, number, t1) ->
+                informationBox.getTractorField().setText(tractor.getPetrolTankFillLevelAsString()));
 
-        harvester.petrolTankFillLevel().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-                informationBox.getHarvesterField().setText(harvester.getPetrolTankFillLevelAsString());
-            }
-        });
+        harvester.petrolTankFillLevel().addListener((observableValue, number, t1) ->
+                informationBox.getHarvesterField().setText(harvester.getPetrolTankFillLevelAsString()));
 
-        silo.capacity().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-                informationBox.getSiloField().setText(silo.getCapacityAsString());
-            }
-        });
-
-
+        silo.capacity().addListener((observableValue, number, t1) ->
+                informationBox.getSiloField().setText(silo.getCapacityAsString()));
     }
-
-
-
 }
