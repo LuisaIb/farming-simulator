c80895d1ce9fd8c7bcd4a86e67634130e54bf533
Luisa Ibele <luisa.ibele@gmail.com>
2021-09-17 11:17:16 +0200

    adding comments

--- src/main/java/gameboard/tiles/CourtTrade.java
+++ src/main/java/gameboard/tiles/CourtTrade.java
@@ -1,9 +1,19 @@
 package gameboard.tiles;
 
+/**
+ * this class represents the Court Trade, where the farmer can sell his grain
+ * to earn money.
+ * @author Luisaibele, Judith
+ *
+ */
 public class CourtTrade extends TileWithBuilding{
     private double sellingPrice;
 
 
+    /**
+     * Constructor of CourtTrade
+     * @param level
+     */
     public CourtTrade(int level) {
         super();
         if (level == 1) {
@@ -15,10 +25,18 @@ public class CourtTrade extends TileWithBuilding{
         }
     }
 
+    /**
+     * Getter for SellingPrice
+     * @return sellingPrice
+     */
     public double getSellingPrice() {
         return sellingPrice;
     }
 
+    /**
+     * Setter for SellingPrice
+     * @param sellingPrice
+     */
     public void setSellingPrice(double sellingPrice) {
         this.sellingPrice = sellingPrice;
     }
