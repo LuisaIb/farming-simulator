352532cd973b13817eac79d13a595ea130b7e79c
Judith Romer <romer.j@stud.dhbw-ravensburg.de>
2021-09-19 21:24:22 +0200

    commenting the class SceneController

--- src/main/java/gui/controller/SceneController.java
+++ src/main/java/gui/controller/SceneController.java
@@ -9,16 +9,20 @@ import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.stage.Stage;
 import simulator.Game;
-import java.util.concurrent.TimeUnit;
 
 /**
  * This class implements the methods, that are responsible for the change of the scene.
+ *
+ * @author Judith
  */
 public class SceneController {
-    private boolean timerInitiated = false;
     Game game;
 
+    /**
+     * This method starts a new game with level of difficulty easy.
+     */
     public EventHandler<Event> setSceneToEasyGameScene = event -> {
+        // to change the scene the actual stage is needed
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         game = new Game();
         GameScene gameScene = game.createNewGame(1);
@@ -26,6 +30,9 @@ public class SceneController {
         window.show();
     };
 
+    /**
+     * This method starts a new game with level of difficulty medium.
+     */
     public EventHandler<Event> setSceneToMediumGameScene = event -> {
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         game = new Game();
@@ -34,6 +41,9 @@ public class SceneController {
         window.show();
     };
 
+    /**
+     * This method starts a new game with level of difficulty hard.
+     */
     public EventHandler<Event> setSceneToHardGameScene = event -> {
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         game = new Game();
@@ -42,6 +52,9 @@ public class SceneController {
         window.show();
     };
 
+    /**
+     * This method reloads the saved game.
+     */
     public EventHandler<Event> setSceneToReloadGameScene = event -> {
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         game = new Game();
@@ -50,6 +63,10 @@ public class SceneController {
         window.show();
     };
 
+    /**
+     * This method sets the scene to the scene in which the player chooses the level of difficulty when starting a new
+     * game.
+     */
     public EventHandler<Event> setSceneToDifficultyScene = event -> {
         DifficultyScene difficultyScene = new DifficultyScene();
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -57,6 +74,10 @@ public class SceneController {
         window.show();
     };
 
+    /**
+     * This method sets the scene back to the menu scene. It is needed in the difficultyScene and the helpScene for the
+     * buttons back.
+     */
     public EventHandler<Event> setSceneToMenuScene = event -> {
         MenuScene menuScene = new MenuScene();
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -64,6 +85,9 @@ public class SceneController {
         window.show();
     };
 
+    /**
+     * This method sets the scene to the helpScene.
+     */
     public EventHandler<Event> setSceneToHelpScene = event -> {
         HelpScene helpScene = new HelpScene();
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -71,6 +95,9 @@ public class SceneController {
         window.show();
     };
 
+    /**
+     * This method ends the game by closing the stage.
+     */
     public EventHandler<Event> endGame = event -> {
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         window.close();
