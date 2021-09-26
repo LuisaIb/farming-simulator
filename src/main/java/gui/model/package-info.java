68564d118e03cd3a141ee2fc71b4d853d45c4744
Judith Romer <romer.j@stud.dhbw-ravensburg.de>
2021-09-26 17:50:09 +0200

    commenting the package model - should now be completely commented

--- src/main/java/gui/model/package-info.java
+++ src/main/java/gui/model/package-info.java
@@ -2,6 +2,6 @@
  * This package contains classes for individualized buttons and text fields as well as a class that is responsible for
  * the information shown on the GUI and a class that helps to load the images.
  *
- * @author Judith Romer
+ * @author Judith
  */
 package gui.model;
