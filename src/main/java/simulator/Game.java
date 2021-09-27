080bd56a510c93481448dc2693145b83c63eece3
Judith Romer <romer.j@stud.dhbw-ravensburg.de>
2021-09-27 13:32:24 +0200

    adding some comments to the classes of the simulator package

--- src/main/java/simulator/Game.java
+++ src/main/java/simulator/Game.java
@@ -18,119 +18,145 @@ import gui.view.GameScene;
 import gui.view.Matchfield;
 
 /**
- * this class deals with staring a new game and reloading an old game status. 
+ * This class offers methods to start a new game and reloading an old game status.
  * @author Isabel, Judith
  *
  */
 public class Game {
-    MovingObject movingObject = new MovingObject();
-    private Farmer farmer;  
+	/**
+	 * an object of the class MovingObject to hold the different moving objects in the game
+	 */
+    MovingObject movingObject;
+	/**
+	 * an object of the class Farmer that is initialized in the methods to start or reload a game
+	 */
+	private Farmer farmer;
+	/**
+	 * an object of the class Tractor that is initialized in the methods to start or reload a game
+	 */
     private Tractor tractor;
+	/**
+	 * an object of the class Harvester that is initialized in the methods to start or reload a game
+	 */
     private Harvester harvester;
+	/**
+	 * an object of the class Cultivator that is initialized in the methods to start or reload a game
+	 */
     private Cultivator cultivator;
+	/**
+	 * an object of the class DumpTruck that is initialized in the methods to start or reload a game
+	 */
     private DumpTruck dumpTruck;
+	/**
+	 * an object of the class SeedDrill that is initialized in the methods to start or reload a game
+	 */
     private SeedDrill seedDrill;
+	/**
+	 * an object of the class FieldTile that is initialized in the methods to start or reload a game
+	 */
     private FieldTile fieldTile;
+	/**
+	 * an object of the class CourtTrade that is initialized in the methods to start or reload a game
+	 */
     private CourtTrade courtTrade;
+	/**
+	 * integer to save a value depending on the moving object that is selected
+	 */
     private int selectedObject;
+	/**
+	 * an object of the class GameValue that is initialized in the methods to start or reload a game
+	 */
     private GameValue gameValue;
+	/**
+	 * an object of the class LevelOfDifficulty that is initialized in the methods to start or reload a game
+	 */
     private LevelOfDifficulty levelOfDifficulty;
+	/**
+	 * an object of the class Silo that is initialized in the methods to start or reload a game
+	 */
     private Silo silo;
+	/**
+	 * an object of the class GameScene that is set to the specific GameScene that is implemented when starting or
+	 * reloading a game
+	 */
     private GameScene gameScene = new GameScene();
+	/**
+	 * an object of the class GameController that is initialized in the methods to start or reload a game
+	 */
 	GameController gameController;
 
 	/**
 	 * This method deals with starting a new game, the param Level Of Difficulty has to be transferred.
+	 *
 	 * @param lod
 	 * This param decides how the new game objects: 
-	 * levelofdifficulty, dumptruck, seeddrill, fieldtile, gamevalue, courttrade and silo are implemented.
-	 * the other objects are implemented with an empty constructor.
-	 * @return the gameScene
+	 * levelOfDifficulty, dumpTruck, seedDrill, fieldTile, gameValue, courtTrade and silo are implemented.
+	 * The other objects are implemented with an empty constructor.
+	 *
+	 * @return the actual gameScene
 	 */
 	public GameScene createNewGame(int lod){
 		levelOfDifficulty = new LevelOfDifficulty(lod);
-    	farmer = new Farmer();
-    	tractor = new Tractor();
-        harvester = new Harvester();
-        cultivator = new Cultivator();
-        dumpTruck = new DumpTruck(lod);
-        seedDrill = new SeedDrill();
-        fieldTile = new FieldTile(lod);
-        gameValue = new GameValue(lod);
-        courtTrade = new CourtTrade(lod);
-        silo = new Silo(lod);
+		movingObject = new MovingObject();
+		farmer = new Farmer();
+		tractor = new Tractor();
+		harvester = new Harvester();
+		cultivator = new Cultivator();
+		dumpTruck = new DumpTruck(lod);
+		seedDrill = new SeedDrill();
+		fieldTile = new FieldTile(lod);
+		gameValue = new GameValue(lod);
+		courtTrade = new CourtTrade(lod);
+		silo = new Silo(lod);
+		setMovingObject();
+        gameController = new GameController();
+	    gameScene.initializeGameScene(farmer.isSelected(),tractor.isSelected(),harvester.isSelected(),
+				cultivator.isSelected(),dumpTruck.isSelected(),seedDrill.isSelected(),fieldTile.getGrowthState(),
+				fieldTile.getGrowthState2(), fieldTile.getGrowthState3(), 1, farmer.getX(), farmer.getY(),
+				gameController);
+		MovingObjectController movingObjectController = new MovingObjectController(gameScene, movingObject,
+			gameController, gameValue, farmer, tractor, harvester, cultivator, dumpTruck, seedDrill, fieldTile,
+			silo, courtTrade);
+    	selectedObject = getSelectedObject();
+        fillInformationFields(gameScene, silo, harvester, tractor, gameValue);
+		gameController.initGameLoop(gameScene, fieldTile, gameValue, movingObjectController);
+    	GameInformation gameInformation = new GameInformation(gameScene.getInformationBox(), gameValue, tractor, harvester, silo);
+    	SavingController sc = new SavingController();
+        sc.createFunctionality(gameScene, gameValue, farmer, tractor, harvester, cultivator, dumpTruck, seedDrill,
+				fieldTile, silo, levelOfDifficulty, courtTrade, movingObject);
 
-        	movingObject = farmer;
-        	gameController = new GameController();
-	        gameScene.initializeGameScene(farmer.isSelected(),tractor.isSelected(),harvester.isSelected(),
-					cultivator.isSelected(),dumpTruck.isSelected(),seedDrill.isSelected(),fieldTile.getGrowthState(),
-					fieldTile.getGrowthState2(), fieldTile.getGrowthState3(), 1, farmer.getX(), farmer.getY(),
-					gameController);
-			MovingObjectController movingObjectController = new MovingObjectController(gameScene, movingObject,
-					gameController, gameValue, farmer, tractor, harvester, cultivator, dumpTruck, seedDrill, fieldTile,
-					silo, courtTrade);
-    	    selectedObject = getSelectedObject();
-        	fillInformationFields(gameScene, silo, harvester, tractor, gameValue);
-			gameController.initGameLoop(gameScene, fieldTile, gameValue, movingObjectController);
-    	    GameInformation gameInformation = new GameInformation(gameScene.getInformationBox(), gameValue, tractor, harvester, silo);
-    	    SavingController sc = new SavingController();
-        	sc.createFunctionality(gameScene, gameValue, farmer, tractor, harvester, cultivator, dumpTruck, seedDrill,
-					fieldTile, silo, levelOfDifficulty, courtTrade, movingObject);
-
-        	return gameScene;
+        return gameScene;
     }
 
-
-//    private void getSelectedObject(Farmer farmer, Tractor tractor, Harvester harvester, Cultivator cultivator,
-//								   DumpTruck dumpTruck, SeedDrill seedDrill){
-//		if (farmer.isSelected()){
-//			selectedObject = 1;
-//		} else if (tractor.isSelected()) {
-//			if (cultivator.isSelected()) {
-//				selectedObject = 4;
-//			} else if (dumpTruck.isSelected()){
-//				if (dumpTruck.getGrainFillLevel() == 0) {
-//					selectedObject = 5;
-//				} else {
-//					selectedObject = 6;
-//				}
-//			} else if (seedDrill.isSelected()) {
-//				selectedObject = 7;
-//			} else {
-//				selectedObject = 2;
-//			}
-//		} else if (harvester.isSelected()) {
-//			selectedObject = 3;
-//		}
-//	}
-
 	/**
-	 * this method is there to get the X-Coordinate of the selected Moving Object
-	 * @param movingObject
-	 * @return
+	 * This method returns the x-value of the moving object.
+	 *
+	 * @param movingObject the MovingObject object of the actual game
+	 * @return the requested x-value of the moving object
 	 */
 	private int getX(MovingObject movingObject){
 		return movingObject.getX();
 	}
 
 	/**
-	 * this method is there to get the Y-Coordinate of the selected Moving Object
-	 * @param movingObject
-	 * @return
+	 * This method returns the y-value of the moving object.
+	 *
+	 * @param movingObject the MovingObject object of the actual game
+	 * @return the requested y-value of the moving object
 	 */
 	private int getY(MovingObject movingObject){
 		return movingObject.getY();
 	}
 
-
 	/**
-	 * this method is there to fill the information fields in the game scene
-	 * with the belonging properties
-	 * @param gameScene
-	 * @param silo
-	 * @param harvester
-	 * @param tractor
-	 * @param gameValue
+	 * This method fills the text fields of the informationBox of the gameScene with the starting values of the game.
+	 * As there are bindings implemented to the properties they change automatically.
+	 *
+	 * @param gameScene the GameScene object of the actual game
+	 * @param silo the Silo object of the actual game
+	 * @param harvester the Harvester object of the actual game
+	 * @param tractor the Tractor object of the actual game
+	 * @param gameValue the GameValue object of the actual game
 	 */
 	private void fillInformationFields(GameScene gameScene, Silo silo, Harvester harvester, Tractor tractor,
 									   GameValue gameValue){
@@ -143,9 +169,9 @@ public class Game {
 
 
 	/**
-	 * this method is there to determine the currently selected object 
-	 * when reloading the game
-	 * @return
+	 * This method returns an integer that is set depending on the moving object that is selected.
+	 *
+	 * @return the integer of the selected moving object
 	 */
 	public int getSelectedObject(){
         if(farmer.isSelected()){
@@ -161,7 +187,7 @@ public class Game {
 				}
 			} else if (seedDrill.isSelected()) {
         		selectedObject = 7;
-			} else {
+			} else { // if there is no attachment to the tractor
 				selectedObject=2;
 			}
         }else if(harvester.isSelected()){
@@ -171,7 +197,7 @@ public class Game {
     }
 
 	/**
-	 * This method proofs, which moving object is selected, sets it as the moving object, and sets the x- and y-values
+	 * This method checks, which moving object is selected, sets it as the moving object, and sets the x- and y-values
 	 * to the ones of the moving object. This method is used for reloading the game.
 	 */
 	private void setMovingObject(){
@@ -194,10 +220,11 @@ public class Game {
 
 
     /**
-     * this method represents the functionality to reload the game after having saved it
-     * It's the other part of the saving process. The serialized JSON-B in the String Array, is deserialized into a new dedicated object.
-     * All saved information are set in the constructor of the new dedicated object.
-     * @return new gameScene
+     * This method represents the functionality to reload the game after saving and restarting it.
+     * It's the second part of the saving process. The serialized JSON-B in the String Array is deserialized into a
+	 * new dedicated object. All saved information is set to the constructor of the new dedicated object.
+	 *
+     * @return the actual gameScene
      */
     public GameScene reloadGame() {
     	ObjectToPojo otp = new ObjectToPojo();
@@ -242,7 +269,14 @@ public class Game {
 		return gameScene;
     }
 
-    private void fillMatchfieldWithFields(FieldTile fieldTile, Matchfield matchfield) {
+	/**
+	 * This method uses the fillWithField() methods from the class Matchfield to implement the tiles of a field that
+	 * have been processed before saving the game.
+	 *
+	 * @param fieldTile the FieldTile object of the actual game
+	 * @param matchfield the Matchfield object of the actual game
+	 */
+	private void fillMatchfieldWithFields(FieldTile fieldTile, Matchfield matchfield) {
 		matchfield.fillWithField1(fieldTile.getIndexesField1ForMatchfield(),
 				fieldTile.getTilesField1CompletedForMatchfield(), fieldTile.getGrowthState());
 		matchfield.fillWithField2(fieldTile.getIndexesField2ForMatchfield(),
--- src/main/java/simulator/Launcher.java
+++ src/main/java/simulator/Launcher.java
@@ -5,28 +5,26 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 /**
- * this class is meant to be the class to start the game
- * @author Luisaibele, Isabel
+ * This class contains the main mthod to start the game.
+ * @author Luisaibele, Isabel, Judith
  *
  */
 public class Launcher extends Application {
-	
 	/**
-	 * this is the main class to start the game
-	 * @param args
+	 * This is the main class to start the game.
+	 *
+	 * @param args arguments that can be handed to the game when started
 	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	/**
-	 *this method is supposed to start the farmer simulator
+	 * This method starts the farmer simulator by showing the mainStage.
 	 */
 	@Override
 	public void start(Stage stage) throws Exception {
 		ViewManager viewManager = new ViewManager();
 		viewManager.initializeStage();
-	
-		
 	}
 }
